9ff7d13c10a1a0cf2fe516c0d034826ce5dedfb0
cersupus <hakfedor@gmail.com>
2025-06-18 08:28:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function showTab(tab, event) {
     trainModel();
     setupDigitCanvas();
   } else if (tab === "visualization") {
-    content.innerHTML = '<iframe src="visualization/visualization.html" style="width:100%; height:3600px; border:none; border-radius: 12px; @media (max-width: 600px):height:100%;"></iframe>';
+    content.innerHTML = '<iframe src="visualization/visualization.html" style="width:100%; max-height: 100%; display: block; height: 4000px; border:none; border-radius: 12px;"></iframe>';
   } else if (tab === "intro") {
   const intro = document.querySelector('#intro-content');
   content.innerHTML = intro ? intro.innerHTML : '<p>Секция "Введение" не найдена</p>';
